ed16e7fef28b933f68b7497d1d890d84cde7b4fb
davydovaolga <olgita.davydova@gmail.com>
2017-02-10 18:37:36 +0300

    Сделала 2 новых модуля: изменение масштаба и применение фильтра

--- js/form.js
+++ js/form.js
@@ -4,107 +4,63 @@ var uploadOverlay = document.querySelector('.upload-overlay');
 var uploadSelectImage = document.querySelector('#upload-select-image');
 var uploadfile = document.querySelector('#upload-file');
 var uploadCancel = document.querySelector('#upload-cancel');
-var controlsValue = document.querySelector('.upload-resize-controls-value');
-var controls = document.querySelectorAll('input[name="upload-filter"]');
-var buttonDec = document.querySelector('.upload-resize-controls-button-dec');
-var buttonInc = document.querySelector('.upload-resize-controls-button-inc');
+var resizeControls = document.querySelector('.upload-resize-controls');
 var uploadControl = document.querySelector('.upload-control');
-var uploadFilterControls = document.querySelector('.upload-filter-controls');
-var filterImagePreview = document.querySelector('.filter-image-preview');
 var KEY_CODE_ENTER = 13;
 var KEY_CODE_ESC = 27;
 
 uploadOverlay.classList.add('invisible');
 uploadSelectImage.classList.remove('invisible');
 
-uploadfile.addEventListener('click', openWindowWithPhoto);
-uploadCancel.addEventListener('click', closeWindowWithPhoto);
-buttonDec.addEventListener('click', zoomOut);
-buttonInc.addEventListener('click', zoomIn);
-uploadFilterControls.addEventListener('click', onChangesFilter);
-uploadFilterControls.addEventListener('keydown', onKeydownEnter);
-uploadCancel.addEventListener('keydown', onEnterCloseWindow);
-uploadControl.addEventListener('keydown', onEnterOpenWindow);
+uploadfile.addEventListener('click', onOpen);
+uploadCancel.addEventListener('click', onClose);
+uploadCancel.addEventListener('keydown', onCloseByEnter);
+uploadControl.addEventListener('keydown', onOpenByEnter);
 
-function onKeydownEnter(evt) {
-  if (evt.keyCode === KEY_CODE_ENTER) {
-    onChangesFilter(event);
+window.initializeFilters();
+window.createScale(resizeControls, 25, 100);
+
+function onOpen(event) {
+  event.preventDefault();
+  open();
+}
+
+function onClose(event) {
+  event.preventDefault();
+  close();
+}
+
+function onCloseByEnter(event) {
+  event.preventDefault();
+  if (event.keyCode === KEY_CODE_ENTER) {
+    close();
   }
 }
 
-function onChangesFilter(event) {
-  var target = event.target;
-  if (target.classList.contains('upload-filter-preview')) {
-    removeFilter();
-    var filter = target.parentNode.previousElementSibling;
-    filterImagePreview.classList.add('filter-' + filter.value);
+function onOpenByEnter(event) {
+  event.preventDefault();
+  if (event.keyCode === KEY_CODE_ENTER) {
+    open();
   }
 }
 
-function onEnterCloseWindow(evt) {
-  if (evt.keyCode === KEY_CODE_ENTER) {
-    closeWindowWithPhoto();
-  }
-}
-
-function onEnterOpenWindow(evt) {
-  if (evt.keyCode === KEY_CODE_ENTER) {
-    openWindowWithPhoto(event);
-  }
-}
-
-function onEscCloseWindow(evnt) {
-  if (evnt.keyCode === KEY_CODE_ESC) {
+function onCloseByEsc(event) {
+  if (event.keyCode === KEY_CODE_ESC) {
     uploadOverlay.classList.add('invisible');
     uploadSelectImage.classList.remove('invisible');
   }
 }
 
-function openWindowWithPhoto(event) {
-  event.preventDefault();
+function open() {
   uploadOverlay.classList.remove('invisible');
   uploadSelectImage.classList.add('invisible');
-  controlsValue.step = 25;
-  controlsValue.value = '100%';
-  var value = parseInt(controlsValue.value, 10);
-  scaleСalculation(value);
-  removeFilter();
-  filterImagePreview.classList.add('filter-none');
   uploadOverlay.ariaHidden = true;
-  document.addEventListener('keydown', onEscCloseWindow);
+  document.addEventListener('keydown', onCloseByEsc);
 }
 
-function removeFilter() {
-  for (var i = 0; i < controls.length; i++) {
-    filterImagePreview.classList.remove('filter-' + controls[i].value);
-  }
-}
-
-function closeWindowWithPhoto() {
+function close() {
   uploadOverlay.classList.add('invisible');
   uploadSelectImage.classList.remove('invisible');
   uploadOverlay.ariaHidden = false;
-  document.removeEventListener('keydown', onEscCloseWindow);
-}
-
-function zoomOut() {
-  var value = parseInt(controlsValue.value, 10);
-  if (value > 25) {
-    var percent = value - 25;
-    scaleСalculation(percent);
-  }
-}
-
-function zoomIn() {
-  var value = parseInt(controlsValue.value, 10);
-  if (value < 100) {
-    var percent = value + 25;
-    scaleСalculation(percent);
-  }
-}
-
-function scaleСalculation(percent) {
-  var scale = percent / 100;
-  filterImagePreview.style.transform = 'scale(' + scale + ')';
-  controlsValue.value = String(percent) + '%';
+  document.removeEventListener('keydown', onCloseByEsc);
 }
